6b042089a50bc307a951d3c0213379041988ccf8
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-06-10 12:59:07 +0800

    调整重要待办色彩

--- src/renderer/components/Todo.vue
+++ src/renderer/components/Todo.vue
@@ -760,7 +760,7 @@ input::-webkit-input-placeholder {
 }
 
 .important-text {
-  color: rgb(252, 202, 156);
+  color: rgb(247, 166, 90);
   font-family: "Arial","Microsoft YaHei","黑体","宋体",sans-serif;
   cursor: default;
 }
